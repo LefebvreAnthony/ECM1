b23b41a7672a1e55e7fee1e1bca8b3236a8fb123
LefebvreAnthony <anthony.lfb@hotmail.fr>
2022-05-04 12:39:38 +0200

    add php variable

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 "use-strict";
 
-
+//? Add RegExp function
 function verifForm(input, reg) {
     let regEx = new RegExp(reg, 'g');
     return regEx.test(input.value);
 };
+
+//! add invalid fonction box
+function noValidateForm(firstName, email, object) {
+    let spanName = document.querySelector('.err_name');
+    let spanEmail = document.querySelector('.err_mail');
+    let spanObject = document.querySelector('.err_object');
+
+    !verifForm(firstName, "^[A-Za-z]{3,25}$") ? spanName.innerHTML = "Name invalid" : spanName.innerHTML = "";
+    !verifForm(email, "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$") ? spanEmail.innerHTML = "Email invalid" : spanEmail.innerHTML = "";
+    !verifForm(object, "^[A-Za-z]{3,20}$") ? spanObject.innerHTML = "Object invalid" : spanObject.innerHTML = "";
+}
+
 
 function validateForm() {
     let form = document.querySelector('form');
@@ -33,27 +45,17 @@ function validateForm() {
         ) {
             fetch("./php/postForm.php", init)
                 .then(res => {
-                    console.log(res);
+                    return res.json()
                 })
                 .then(res => {
                     console.log(res);
                 })
         } else {
-
+            alert('Formulaire Invalid !')
         }
 
     })
 };
-
-function noValidateForm(firstName, email, object) {
-    let spanName = document.querySelector('.err_name');
-    let spanEmail = document.querySelector('.err_mail');
-    let spanObject = document.querySelector('.err_object');
-
-    !verifForm(firstName, "^[A-Za-z]{3,25}$") ? spanName.innerHTML = "Name invalid" : spanName.innerHTML = "";
-    !verifForm(email, "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$") ? spanEmail.innerHTML = "Email invalid" : spanEmail.innerHTML = "";
-    !verifForm(object, "^[A-Za-z]{3,20}$") ? spanObject.innerHTML = "Object invalid" : spanObject.innerHTML = "";
-}
 
 
 validateForm()
--- php/postForm.php
+++ php/postForm.php
@@ -1,5 +1,13 @@
 <?php
+session_start();
+//Permet de lire les données brutes du body req
+$request_body = file_get_contents('php://input');
+$data = json_decode($request_body, true);
 
-$name = $_POST["name"];
+$name = $data['name'];
+$email = $data['email'];
+$object = $data['object'];
+$message = $data['message'];
 
-echo $_SESSION;
+session_destroy();
+
